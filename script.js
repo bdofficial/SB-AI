3935806d1ca8f6440e168726c5e4712322b615f6
banglaploa@gmail.com <banglaploa@gmail.com>
2022-09-14 00:21:49 +0600

    555

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ btn.addEventListener("click", () => {
 const readOutLoud = (message) => {
   const speech = new SpeechSynthesisUtterance();
 
-text.innerHTML = message;
+text.innerHTML = message.toLowerCase();
   
 if (message.includes("how are you")) {
     speech.text = "i am not fine";
@@ -33,8 +33,49 @@ if (message.includes("how are you")) {
     speech.volume = 1;
     speech.rate = 0.5;
     speech.pitch = 1.3;
-    window.speechSynthesis.speak(speech);
+    window.speechSynthesis.speak(speech); 
   } 
+else if (message.toLowerCase() == "hello darling") {
+  speech.text = "yes hunny";
+  textinto.innerHTML = speech.text;
+  speech.volume = 1;
+  speech.rate = 0.05;
+  speech.pitch = 1.3;
+  window.speechSynthesis.speak(speech);
+} 
+
+else if (message.includes("can you open app")) {
+  speech.text = "yes i can";
+  textinto.innerHTML = speech.text;
+  speech.volume = 1;
+  speech.rate = 0.4;
+  speech.pitch = 1.4;
+  window.speechSynthesis.speak(speech);
+}
+else if (message.toLowerCase() == "open flashlight" || message.toLowerCase() == "flashlight") {
+  window.open('https://bdurl.netlify.app/fl.html');
+  textinto.innerHTML = "flashlight oppened";
+}
+else if (message.toLowerCase()=="open youtube"|| message.toLowerCase() == "youtube") {
+  window.open('https://youtube.com');
+  textinto.innerHTML = "youtube oppened";
+}
+else if (message.toLowerCase() == "open facebook" || message.toLowerCase() == "facebook") {
+  window.open('https://facebook.com');
+  textinto.innerHTML = "facebook oppened";
+}
+else if (message.toLowerCase() == "open instagram" || message.toLowerCase() == "instagram") {
+  window.open('https://instagram.com');
+  textinto.innerHTML = "instagram oppened";
+}
+else if (message.toLowerCase() == "open twitter" || message.toLowerCase() == "twitter") {
+  window.open('https://twitter.com');
+  textinto.innerHTML = "twitter oppened";
+}
+else if (message.toLowerCase() == "open tiktok" || message.toLowerCase() == "tiktok") {
+  window.open('https://tiktok.com');
+  textinto.innerHTML = "tiktok oppened";
+}
 else if (message.includes("can you tell me today's weather")) {
   speech.text = "no i can't";
   textinto.innerHTML = speech.text;
@@ -117,7 +158,7 @@ else if (message.includes("what is your name")) {
 }  
 else {
   speech.text = "please say again";
-  textinto.innerHTML=speech.text;
+  textinto.innerHTML = speech.text;
   speech.volume = 1;
   speech.rate = 0.84;
   speech.pitch = 1.4;
